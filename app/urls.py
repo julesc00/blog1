abfffab8611475c551a34bb5a1b43ae8e7f92e91
Julio Briones <julio@simondata.com>
2023-12-28 09:00:59 -0600

    Before postgresql searching feature.

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 
 from django.urls import path
+
+from .feeds import LatestPostsFeed
 from .views import (post_detail, post_share, post_list)
 
 app_name = "blog"
@@ -10,4 +12,5 @@ urlpatterns = [
     path("<int:year>/<int:month>/<int:day>/<slug:post_msg>", post_detail, name="post_detail"),
     path("<int:post_id>/share/", post_share, name="post_share"),
     path("tag/<slug:tag_slug>/", post_list, name="post_list_by_tag"),
+    path("feed/", LatestPostsFeed(), name="post_feed"),
 ]
